176b58184f9c1dbb7df3fe7cf39cb9714c05d60e
Trix <trixie@openmailbox.org>
2017-07-03 19:14:52 +1000

    Finish sketch tools

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# <strong>PROJECT: CREATE PIXEL SKETCH</strong>
+# <strong>PROJECT: CREATE SKETCHPAD</strong>
 
 From The Odin Project's [curriculum](https://www.theodinproject.com/courses/web-development-101/lessons/javascript-and-jquery)
+
+- [x] Design user interface
+- [x] Make a div creating script for pad
+- [x] Create functional tools 
+- [] Add more colors
+- [] Replace scale button with jQuery UI slider
+- [] Do it all with a kitty on lap <3 
--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,83 @@
-var userInput = 16;
+var gridSize = 20;
+var drawOn = true;
 
-function createGrid() {
-	//create divs to cover x-axis
-	//create divs to cover y-axis
-	//fill in the blanks
-
+function createGrid(num) {
 	$(".board").append('<table></table>');
-	$("tr").append("<td></td>"); // also needs for loop*/
 
-	for (var i = 0; i < userInput; i++) {
+	for (var i = 0; i < num; i++) {
 		$("table").append("<tr></tr>");
 	}
 
-	for (var j = 0; j < userInput; j++) {
+	for (var j = 0; j < num; j++) {
 		$("tr").append("<td></td>");
 	}
+}
 
+
+function changeGrid() {
+	var sizeInput = prompt("How big do you want it?", gridSize);
+
+	if (sizeInput >= 1 && sizeInput <= 100) {
+		$(".board > table").remove();
+		createGrid(sizeInput);
+	} else if (sizeInput > 100) {
+		alert("Stahp, it hurts...");
+	} else if (isNaN(sizeInput)) {
+		alert("Error: Input is not a number.");
+	};
+}
+
+
+function drawCheck() {
+	if (drawOn) {
+		$("td").on("mouseenter", function(){
+			$(this).addClass("whitechalk");
+		});
+	} else {
+		$("td").on("mouseenter", function(){
+			$(this).removeClass("whitechalk");
+		});
+	}
+}
+
+function drawTrue() {
+	drawOn = true;
+}
+
+function drawFalse() {
+	drawOn = false;
 }
 
 
 $(document).ready(function(){
+
+	createGrid(gridSize);
+
+	drawCheck();
 	
+	$(".scale").on("click", function(){
+		changeGrid();
+		drawCheck();
+	});
+
+	$(".clear").on("click", function(){
+		$("td").removeClass("whitechalk");
+		drawCheck();
+	});
+
+	$(".draw").on("click", function(){
+		drawTrue();
+		drawCheck();
+	});
+
+	$(".erase").on("click", function(){
+		drawFalse();
+		drawCheck();
+	});
+
+
+	/*--------HOVERS-------*/
+
 	$('.erase').hover(
 	function() {
 		$(".eraselabel").addClass("appear");
@@ -53,6 +110,5 @@ $(document).ready(function(){
 		$(this).removeClass("darker");
 	});
 
-
-	createGrid();
+	
 });
