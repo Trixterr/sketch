67016c99288a703989dd39c4f18327c57e1e2061
Trix <trixie@openmailbox.org>
2017-07-05 22:01:21 +1000

    Create rainbow. Change background

--- README.md
+++ README.md
@@ -5,12 +5,13 @@ From The Odin Project's [curriculum](https://www.theodinproject.com/courses/web-
 - [x] Design user interface
 - [x] Make a div creating script for pad
 - [x] Create functional tools 
-- [] Add more colors
--- [x] Red
--- [] Orange
--- [] Yellow
--- [] Green 
--- [] Blue 
--- [] Purple
+- [x] Add more colors
+- [] Add rainbow tool
+- [] Replace labels with active tool indicator
 - [] Replace scale button with jQuery UI slider
+- [] Change hover to click ><
 - [] Do it all with a kitty on lap <3 
+
+### NOTES
+
+I don't know how to make a rainbow label. :( 
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 var gridSize = 20;
 var drawOn = "white";
 
+
 function createGrid(num) {
 	$(".board").append('<table></table>');
 
@@ -27,25 +28,104 @@ function changeGrid() {
 	};
 }
 
-
 function drawCheck() {
 	if (drawOn === "white") {
 		$("td").on("mouseenter", function(){
 			$(this).addClass("whitechalk");
 		});
+		$("span").removeClass();
+		$("span").html("white");
+		$("span").addClass("activewhite");
+
 	} else if (drawOn === "red") {
 		$("td").on("mouseenter", function(){
 			$(this).addClass("redchalk");
 		});
+		$("span").removeClass();
+		$("span").html("red");
+		$("span").addClass("activered");
+
+	} else if (drawOn === "orange") {
+		$("td").on("mouseenter", function(){
+			$(this).addClass("orangechalk");
+		});
+		$("span").removeClass();
+		$("span").html("orange");
+		$("span").addClass("activeorange");
+
+	} else if (drawOn === "yellow") {
+		$("td").on("mouseenter", function(){
+			$(this).addClass("yellowchalk");
+		});
+		$("span").removeClass();
+		$("span").html("yellow");
+		$("span").addClass("activeyellow");
+
+	} else if (drawOn === "green") {
+		$("td").on("mouseenter", function(){
+			$(this).addClass("greenchalk");
+		});
+		$("span").removeClass();
+		$("span").html("green");
+		$("span").addClass("activegreen");
+
+	} else if (drawOn === "blue") {
+		$("td").on("mouseenter", function(){
+			$(this).addClass("bluechalk");
+		});
+		$("span").removeClass();
+		$("span").html("blue");
+		$("span").addClass("activeblue");
+
+	} else if (drawOn === "purple") {
+		$("td").on("mouseenter", function(){
+			$(this).addClass("purplechalk");
+		});
+		$("span").removeClass();
+		$("span").html("purple");
+		$("span").addClass("activepurple");
+
+	} else if (drawOn === "rainbow") {
+		$("span").removeClass();
+		$("span").html("rainbow");
+		$("span").addClass("activeeraser");
+
 	} else {
 		$("td").on("mouseenter", function(){
 			$(this).removeClass();
 		});
+		$("span").removeClass();
+		$("span").html("eraser");
+		$("span").addClass("activeeraser");
 	}
 }
 
 function drawRed() {
 	drawOn = "red";
+}
+
+function drawOrange() {
+	drawOn = "orange";
+}
+
+function drawYellow() {
+	drawOn = "yellow";
+}
+
+function drawGreen() {
+	drawOn = "green";
+}
+
+function drawBlue() {
+	drawOn = "blue";
+}
+
+function drawPurple() {
+	drawOn = "purple";
+}
+
+function drawRainbow() {
+	drawOn = "rainbow";
 }
 
 function drawWhite() {
@@ -91,6 +171,42 @@ $(document).ready(function(){
 		drawCheck();
 	});
 
+	$(".draworange").on("click", function(){
+		drawClear()
+		drawOrange();
+		drawCheck();
+	});
+
+	$(".drawyellow").on("click", function(){
+		drawClear()
+		drawYellow();
+		drawCheck();
+	});
+
+	$(".drawgreen").on("click", function(){
+		drawClear()
+		drawGreen();
+		drawCheck();
+	});
+
+	$(".drawblue").on("click", function(){
+		drawClear()
+		drawBlue();
+		drawCheck();
+	});
+
+	$(".drawpurple").on("click", function(){
+		drawClear()
+		drawPurple();
+		drawCheck();
+	});
+
+	$(".drawrainbow").on("click", function(){
+		drawClear();
+		drawRainbow();
+		drawCheck();
+	});
+
 	$(".erase").on("click", function(){
 		drawFalse();
 		drawCheck();
@@ -98,30 +214,6 @@ $(document).ready(function(){
 
 
 	/*--------HOVERS-------*/
-
-	$('.erase').hover(
-	function() {
-		$(".eraselabel").addClass("appear");
-	}, 
-	function() {
-		$(".eraselabel").removeClass("appear");
-	});
-
-	$(".drawwhite").hover(
-	function() {
-		$(".drawwhitelabel").addClass("appear");
-	}, 
-	function() {
-		$(".drawwhitelabel").removeClass("appear");
-	});
-
-	$(".drawred").hover(
-	function() {
-		$(".drawredlabel").addClass("appear");
-	}, 
-	function() {
-		$(".drawredlabel").removeClass("appear");
-	});
 
 	$(".scale").hover(
 	function() {
@@ -139,5 +231,4 @@ $(document).ready(function(){
 		$(this).removeClass("darker");
 	});
 
-	
 });
